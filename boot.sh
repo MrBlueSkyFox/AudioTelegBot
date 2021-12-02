161f238a41a4bbbdd379d61909cdb819b369a519
MrBlueSkyFox <31796400+MrBlueSkyFox@users.noreply.github.com>
2021-12-02 20:44:09 +0300

    Delete boot.sh

--- boot.sh
+++
@@ -1,2 +0,0 @@
-#wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
-curl --url 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml'
